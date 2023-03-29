2c538690ba4d4a3b68dce65d6788754635ea9e48
Tymofii-Shtyrba <timofeyshtyrba@gmail.com>
2023-03-29 03:42:22 +0300

    Update gyroscope.js

--- js/gyroscope.js
+++ js/gyroscope.js
@@ -3,9 +3,9 @@ const axises = document.querySelectorAll('span');
 let gyroscope = new Gyroscope({frequency: 100});
 
 gyroscope.addEventListener('reading', e => {
-  axises[0].textContent = `Axis X: ${(gyroscope.x * 100).toFixed}`;
-  axises[0].textContent = `Axis Y: ${(gyroscope.y * 100).toFixed}`;
-  axises[0].textContent = `Axis Z: ${(gyroscope.z * 100).toFixed}`;
+  axises[0].textContent = `Axis X: ${(gyroscope.x * 100).toFixed()}`;
+  axises[0].textContent = `Axis Y: ${(gyroscope.y * 100).toFixed()}`;
+  axises[0].textContent = `Axis Z: ${(gyroscope.z * 100).toFixed()}`;
 
 
 });
